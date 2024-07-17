0643cfeaa02c932eafcf857239261aaf982b62d4
Kropivtseva_Daria <kropivtseva.daria@gmail.com>
2024-07-17 15:45:42 +0300

    Stars

--- src/Stars.java
+++ src/Stars.java
@@ -7,6 +7,10 @@ public class Stars {
             st5();
             st6();
             st7();
+            st8();
+            st9();
+            st10();
+            st11();
         }
 
         public static void st1() {
@@ -107,4 +111,63 @@ public class Stars {
                 System.out.println("  ");
             }
         }
+
+    public static void st8() {
+        int n = 9; // Adjusted to make the pattern symmetric
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || i == n - 1 || j == 0 || j == n - 1 ||
+                    (i == 2 && j > 1 && j < n - 2) || (i == n - 3 && j > 1 && j < n - 2) ||
+                    (j == 2 && i > 1 && i < n - 2) || (j == n - 3 && i > 1 && i < n - 2)) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
     }
+
+    public static void st9() {
+        int n = 10;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j || i + j == n - 1) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void st10() {
+        int n = 10;
+        int m = n / 2;
+        for (int i = -m; i <= m; i++) {
+            for (int j = -m; j <= m; j++) {
+                if (Math.abs(i) + Math.abs(j) <= m) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void st11() {
+        int n = 10;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i <= j && i <= n - j - 1 || i >= j && i >= n - j - 1) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
